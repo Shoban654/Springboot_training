443e621fda9d294b88ea86814ef3471e7b7df7be
Shoban654 <71747545+Shoban654@users.noreply.github.com>
2022-12-06 22:19:08 +0530

    Add files via upload

--- ISP.java
+++ ISP.java
@@ -1,15 +1,15 @@
 import java.util.*;
 
-public interface student{
-    public void max_height();
-    public void avg_height();
-    public void min_height();
+interface student{
+    public void max_height(List<Integer> student_height);
+    public void avg_height(List<Integer> student_height);
+    public void min_height(List<Integer> student_height);
 }
 class height_max implements student{
-    List<int> student_height=new ArrayList<int>();
-    public void max_height(List<int> student_height)
+    List<Integer> student_height=new ArrayList<Integer>();
+    public void max_height(List<Integer> student_height)
     {
-        this.List<int> student_height;
+        this.student_height=student_height;
         int max=0;
         for (int i=0; i<student_height.size();i++)
         {
@@ -20,15 +20,15 @@ class height_max implements student{
         }
         System.out.println(max);
     }
-    public void avg_height(List<int> student_height);
-    public void min_height(List<int> student_height);
+    public void avg_height(List<Integer> student_height){}
+    public void min_height(List<Integer> student_height){}
 }
 class height_min implements student{
-    List<int> student_height;
-    public void max_height(List<int> student_height);
-    public void avg_height(List<int> student_height);
+    List<Integer> student_height;
+    public void max_height(List<Integer> student_height){}
+    public void avg_height(List<Integer> student_height){}
 
-    public void min_height(List<int> student_height){
+    public void min_height(List<Integer> student_height){
         this.student_height=student_height;
         int min=0;
         for (int i=0; i<student_height.size();i++)
@@ -43,18 +43,24 @@ class height_min implements student{
 }
 
 public class ISP {
-    public static void main(String[] args)
+
+    public void main(String[] args)
     {
         Scanner scan=new Scanner(System.in);
-        List<int> student_height= new ArrayList<int>();
+        List<Integer> student_height= new ArrayList<Integer>();
 
         while (scan.hasNextInt()) {
-            String line = scan.next();
+            Integer line = scan.nextInt();
             if (line.isEmpty()) {
                 break;
             }
             student_height.add(line);
         }
+        height_min obj1=new height_min();
+        obj1.min_height(student_height);
+        height_max obj2=new height_max();
+        obj2.max_height(student_height);
     }
+    
 
 }
